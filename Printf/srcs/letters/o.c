1f86a6a9cd9c807463284846018436c430c5a861
Jules <jules.spender@epitech.eu>
2017-11-27 11:58:00 +0100

    Clean valgrind, patch note, for o

--- Printf/srcs/letters/o.c
+++ Printf/srcs/letters/o.c
@@ -20,13 +20,14 @@ int		o(va_list argp, const char *arg, int *index, t_flag *flag)
 	(void)arg;
 	(void)index;
 	length_modif_uox(argp, flag, &sent);
-	tmp = ft_getnbr_base(sent, "01234567");
+	tmp = ft_getnbr_base_ui(sent, "01234567");
 	flag->larg = flag->larg - ft_strlen(tmp);
 	larg_flag_before(flag);
 	if (ft_strlen(tmp) < flag->point && flag->point != 0)
 		while (ft_strlen(tmp) < flag->point--)
 			ft_putchar('0', flag);
 	ft_putstr(tmp, flag);
+	free (tmp);
 	larg_flag_after(flag);
 	return (0);
 }
