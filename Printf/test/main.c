1d04dd94b50cb9f79cf3bc9bd2331642b522e9a3
Jules <jules.spender@epitech.eu>
2017-11-29 00:04:16 +0100

    To debug a little and will be done

--- Printf/test/main.c
+++ Printf/test/main.c
@@ -17,11 +17,11 @@
 
 int		main(int argc, char **argv)
 {
-	int		i1;
-	int		i2;
+//	int		i1;
+//	int		i2;
 
-	i1 = printf("(%5.12d)\n", 128);
-	i2 = ft_printf("(%5.12d)\n", 128);
-	printf("Printf = %d\nFt_Printf = %d\n", i1, i2);
+//	i1 = printf("(%5.12d)\n", 128);
+	ft_printf("%d\n", 128);
+//	printf("Printf = %d\nFt_Printf = %d\n", i1, i2);
 	return (0);
 }
