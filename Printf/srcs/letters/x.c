2608ba78915a6725250fdf50f6f9e9efb744627e
Jules SPENDER <juspende@e2r11p12.42.fr>
2017-11-30 22:09:44 +0100

    Closely ended

--- Printf/srcs/letters/x.c
+++ Printf/srcs/letters/x.c
@@ -6,7 +6,7 @@
 /*   By: juspende <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 11:18:40 by juspende          #+#    #+#             */
-/*   Updated: 2017/11/30 17:52:58 by juspende         ###   ########.fr       */
+/*   Updated: 2017/11/30 22:08:16 by juspende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,39 @@ int		x(va_list argp, const char *arg, int *index, t_flag *flag)
 {
 	uintmax_t		tmp;
 	char			*fnl;
+	int				i;
+	int				b;
 
+	i = -1;
+	b = 0;
 	(void)index;
 	length_modif_uox(argp, flag, &tmp);
 	if ((char)arg[0] == 'X')
 		fnl = ft_getnbr_base_ui(tmp, "0123456789ABCDEF");
 	else
 		fnl = ft_getnbr_base_ui(tmp, "0123456789abcdef");
-	if (fnl[0] != '0')
-		flag->larg = flag->larg - ft_strlen(fnl);
-	(flag->diez != 0 ? flag->larg = flag->larg - 2 : flag->larg);
-	if (flag->zero == 0)
+	if (flag->comma && !flag->point && fnl[0] == '0')
+		b = -1;
+	flag->tilt = ft_strlen(fnl);
+	flag->larg -= ft_strlen(fnl);
+	flag->point -= ft_strlen(fnl);
+	flag->point < 0 ? (flag->point = 0) : flag->point;
+	flag->larg -= flag->point;
+	flag->diez ? flag->larg = flag->larg - 2 : flag->larg;
+	b == -1 ? flag->larg += 1 : flag->larg;
+	if (!flag->zero)
 		larg_flag_before(flag);
 	if (flag->diez != 0 && fnl[0] != '0')
 	{
 		ft_putchar('0', flag);
 		ft_putchar(arg[0], flag);
 	}
-	if (flag->zero != 0)
+	if (flag->zero)
 		larg_flag_before(flag);
-	ft_putstr(fnl, flag);
+	while (flag->point-- > 0)
+		ft_putchar('0', flag);
+	if (b != -1)
+		ft_putnstr(fnl, flag, &i);
 	larg_flag_after(flag);
 	free (fnl);
 	return (0);
--- Printf/test/main.c
+++ Printf/test/main.c
@@ -6,7 +6,7 @@
 /*   By: juspende <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 12:47:34 by juspende          #+#    #+#             */
-/*   Updated: 2017/11/30 21:43:31 by juspende         ###   ########.fr       */
+/*   Updated: 2017/11/30 22:06:44 by juspende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int		main(int argc, char **argv)
 	int		i2;
 	int		i1;
 
-	i2 = printf("Ur Printf:{%-15Z} \n", 123);
-	i1 = ft_printf("My Printf:{%-15Z} \n", 123);
+	i2 = printf("Ur Printf:@moulitest: %#08x", 42);
+	i1 = ft_printf("My Printf:@moulitest: %#08x", 42);
 	printf("Printf = %d, Ft_printf = %d\n", i2, i1);
 	return (0);
 }
