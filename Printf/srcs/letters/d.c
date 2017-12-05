ac59949e4b96b8210910d2054b935d93358ac3ae
Jules SPENDER <juspende@e2r10p15.42.fr>
2017-12-05 20:25:48 +0100

    Push

--- Printf/srcs/letters/d.c
+++ Printf/srcs/letters/d.c
@@ -6,7 +6,7 @@
 /*   By: juspende <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 11:18:40 by juspende          #+#    #+#             */
-/*   Updated: 2017/12/05 18:43:36 by juspende         ###   ########.fr       */
+/*   Updated: 2017/12/05 20:23:53 by juspende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,57 +115,4 @@ int			d(va_list argp, const char *arg, int *index, t_flag *flag)
 		ft_putnstr_free(ft_itoa(ptr, 10), flag, &c);
 	larg_flag_after_d(flag);
 	return (0);
-}
-
-int			int_len(intmax_t nbr)
-{
-	uintmax_t	tmp;
-
-	if (nbr < 0)
-		tmp = (uintmax_t)-nbr;
-	else
-		tmp = (uintmax_t)nbr;
-	if (tmp >= 1000000000000000000)
-		return (19);
-	else if (tmp >= 100000000000000000)
-		return (18);
-	else if (tmp >= 10000000000000000)
-		return (17);
-	else if (tmp >= 1000000000000000)
-		return (16);
-	else if (tmp >= 100000000000000)
-		return (15);
-	else if (tmp >= 10000000000000)
-		return (14);
-	else if (tmp >= 1000000000000)
-		return (13);
-	else if (tmp >= 100000000000)
-		return (12);
-	else if (tmp >= 10000000000)
-		return (11);
-	return (int_len2(tmp));
-}
-
-int			int_len2(uintmax_t nbr)
-{
-	if (nbr >= 1000000000)
-		return (10);
-	else if (nbr >= 100000000)
-		return (9);
-	else if (nbr >= 10000000)
-		return (8);
-	else if (nbr >= 1000000)
-		return (7);
-	else if (nbr >= 100000)
-		return (6);
-	else if (nbr >= 10000)
-		return (5);
-	else if (nbr >= 1000)
-		return (4);
-	else if (nbr >= 100)
-		return (3);
-	else if (nbr >= 10)
-		return (2);
-	else
-		return (1);
 }
