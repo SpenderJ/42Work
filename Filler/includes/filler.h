d8a2f9826fa0fe3cb7e10e6014bc3f1d354f7358
Jules Spender <pro.jspender@gmail.com>
2017-12-07 17:48:17 +0100

    J AIME LES MACROS C VRMT TROP BO JE PENSE UAEL A EU RAISON DE MOI FAIS CHIER EN PLUS EYWEK IL A VALIDER LS FAUT JLE RATTRAPE GO TRYHARD

--- Filler/includes/filler.h
+++ Filler/includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: juspende <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 14:28:47 by juspende          #+#    #+#             */
-/*   Updated: 2017/12/07 15:52:57 by juspende         ###   ########.fr       */
+/*   Updated: 2017/12/07 17:47:39 by juspende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,18 @@
 
 # ifndef Attack_Succes_Time_To_Launch_The_New_map
 #  define Attack_Succes_Time_To_Launch_The_New_map 1
+# endif
+
+# ifndef SUCCESS
+#  define SUCCESS 1
+# endif
+
+# ifndef FAIL
+#  define FAIL 0
+# endif
+
+# ifndef OPPON_CHAR
+#  define OPPON_CHAR(Value) (Value == 'O' ? 'O' : 'C')
 # endif
 
 # ifndef Keep_Going
@@ -41,6 +53,18 @@
 #  define DOWNLEFT 3
 # endif
 
+# ifndef MAJ
+#  define MAJ 32
+# endif
+
+# ifndef FREE_SPACE
+#  define FREE_SPACE '*'
+# endif
+
+# ifndef One_Or_Nothing
+#  define One_Or_Nothing(Value) (Value == 1 ? 1 : 0)
+# endif
+
 # include <stdio.h>
 # include <fcntl.h>
 # include "get_next_line.h"
@@ -49,7 +73,7 @@
 typedef struct	s_info
 {
 	int		player;
-	int		letter;
+	char	letter;
 	int		finalx;
 	int		finaly;
 	int		ymap;
@@ -70,7 +94,7 @@ typedef struct	s_piece
 
 int		main(void);
 void	ft_get_piece(t_piece *piece);
-void	ft_get_map(t_info *info);
+void	ft_get_map(t_info *info, t_piece *piece);
 int		play_1(t_info *info, t_piece *piece);
 int		play_2(t_info *info, t_piece *piece);
 void	ft_info_player(t_info *info);
---
+++ Filler/srcs/value.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   value.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juspende <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/07 16:48:27 by juspende          #+#    #+#             */
+/*   Updated: 2017/12/07 17:47:37 by juspende         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/filler.h"
+
+static int	check_mate(t_piece *piece, t_info *info, int y, int x)
+{
+	char	c;
+
+	c = info->letter;
+	if (info->map[y][x] == c || info->map[y][x] == MAJ + c)
+		if (piece->piece[y][x] == FREE_SPACE)
+			return (SUCCESS);
+	return (FAIL);
+}
+
+static int ft_occuped(char **map, int y, int x, char c)
+{
+	if (map[y][x] == c)
+		return (SUCCESS);
+	else if (map[y][x] == MAJ + c)
+		return (SUCCESS);
+	return (FAIL);
+}
+
+static int	check_value(t_piece *piece, t_info *info, int i, int j)
+{
+	int		over;
+	int		y;
+	int		x;
+
+	over = 0;
+	y = -1;
+	while (++y < piece->y && (x = -1) == -1)
+		while (++x < piece->x &&
+				(over += check_mate(piece, info, y + i, x + j)))
+			if ((ft_occuped(info->map, y + i, x + j, info->letter) == 1) &&
+					piece->piece[y][x] == FREE_SPACE)
+				return (FAIL);
+	return (One_Or_Nothing(over));
+}
+
+int		try_value(t_piece *piece, t_info *info, int i, int j)
+{
+	if (i + piece->y > info->ymap)
+		return (FAIL);
+	else if (j + piece->x > info->xmap)
+		return (FAIL);
+	else
+		return (check_value(piece, info, i, j));
+}
