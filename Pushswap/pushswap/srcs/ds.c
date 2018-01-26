31c854b317995b0f7973c1b6bc680b7f89dee921
Jules Spender <pro.jspender@gmail.com>
2018-01-26 16:44:49 +0100

    Few fixes, and a lot of code cleared

--- Pushswap/pushswap/srcs/ds.c
+++ Pushswap/pushswap/srcs/ds.c
@@ -6,7 +6,7 @@
 /*   By: juspende <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 13:48:54 by juspende          #+#    #+#             */
-/*   Updated: 2018/01/26 15:34:31 by juspende         ###   ########.fr       */
+/*   Updated: 2018/01/26 16:44:26 by juspende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,16 +48,16 @@ static int	parse_again_with_medians(int *a_list, int *b_list)
 	/* MED_TABLE INITIALIZATION DONE
 	*/
 
-	ft_printf("RECCURSIVE Debug of the reccursive\n");
+	//	ft_printf("RECCURSIVE Debug of the reccursive\n");
 	while (b_list[0] >= 2)
 	{
 		++med_table[MED_NUM];
 		med_table[med_table[MED_NUM]] = (ft_intmax(b_list) + ft_intmin(b_list)) / 2;
 		init = b_list[1];
-		ft_printf("RECCURSIVE A_list :");
-		ft_printint(a_list);
-		ft_printf("RECCURSIVE Med_Table = ");
-		ft_printint(med_table);
+		//		ft_printf("RECCURSIVE A_list :");
+		//		ft_printint(a_list);
+		//		ft_printf("RECCURSIVE Med_Table = ");
+		//		ft_printint(med_table);
 		while (b_list[b_list[0]] != init)
 		{
 			if (b_list[b_list[0]] > med_table[med_table[MED_NUM]] &&
@@ -66,8 +66,8 @@ static int	parse_again_with_medians(int *a_list, int *b_list)
 			else if (ft_publish(RB) != S_ERR)
 				rb(b_list);
 		}
-		ft_printf("RECCURSIVE B_list :");
-		ft_printint(b_list);
+		//		ft_printf("RECCURSIVE B_list :");
+		//		ft_printint(b_list);
 	}
 	--med_table[MED_NUM];
 	if (b_list[0] == 1 && ft_publish(PA) != S_ERR &&
@@ -80,12 +80,12 @@ static int	parse_again_with_medians(int *a_list, int *b_list)
 	}
 	else
 	{
-		ft_printf("RECCURSIVE Etat des listes A:");
-		ft_printint(a_list);
-		ft_printf("RECCURSIVE liste B:");
-		ft_printint(b_list);
-		ft_printf("RECCURSIVE Median :");
-		ft_printint(med_table);
+		//		ft_printf("RECCURSIVE Etat des listes A:");
+		//		ft_printint(a_list);
+		//		ft_printf("RECCURSIVE liste B:");
+		//		ft_printint(b_list);
+		//		ft_printf("RECCURSIVE Median :");
+		//		ft_printint(med_table);
 		return (ft_putsterr(UNKNOWN_BUG));
 	}
 	while (med_table[MED_NUM] > 1)
@@ -100,13 +100,13 @@ static int	parse_again_with_medians(int *a_list, int *b_list)
 		}
 		if (b_list[0] != EMPTY)
 		{
-			ft_printf("[Etat avant crash]:\n\n");
-			ft_printf("Med_Table :");
-			ft_printint(med_table);
-			ft_printf("A_list :");
-			ft_printint(a_list);
-			ft_printf("B_list :");
-			ft_printint(b_list);
+			//			ft_printf("[Etat avant crash]:\n\n");
+			//			ft_printf("Med_Table :");
+			//			ft_printint(med_table);
+			//			ft_printf("A_list :");
+			//			ft_printint(a_list);
+			//			ft_printf("B_list :");
+			//			ft_printint(b_list);
 			return (ft_putsterr(UNKNOWN_BUG));
 		}
 		else
@@ -115,10 +115,10 @@ static int	parse_again_with_medians(int *a_list, int *b_list)
 			med_table[med_table[MED_NUM] + 1] = 0;
 		}
 	}
-	ft_printf("RECCURSIVE END A_list :");
-	ft_printint(a_list);
-	ft_printf("RECCURSIVE END B_list :");
-	ft_printint(b_list);
+	//	ft_printf("RECCURSIVE END A_list :");
+	//	ft_printint(a_list);
+	//	ft_printf("RECCURSIVE END B_list :");
+	//	ft_printint(b_list);
 	return (SORTED);
 }
 
@@ -176,11 +176,13 @@ int			ds(int *a_list, int *b_list)
 		else if (ft_publish(RA) != S_ERR)
 			ra(a_list);
 	}
+	while (b_list[0] != EMPTY && ft_publish(PA) != S_ERR)
+		pa(a_list, b_list);
 
 	/* Filling the median table with the first value */
 
-	med_table[1] = summ;
-	++med_table[MED_NUM];
+	//	med_table[1] = summ;
+	//	++med_table[MED_NUM];
 
 	/* Automatising the first half moves for the first part */
 
@@ -197,10 +199,10 @@ int			ds(int *a_list, int *b_list)
 	 ** MEdians define the range, so you have to make "-"
 	 */
 
-	ft_printf("a_list :");
-	ft_printint(a_list);
-	ft_printf("b_list :");
-	ft_printint(b_list);
+	//	ft_printf("a_list :");
+	//	ft_printint(a_list);
+	//	ft_printf("b_list :");
+	//	ft_printint(b_list);
 
 	/* Now it's time to have our main action for the 250 most little numbers (if 500 numbers sent).
 	 ** This while is waiting that everything is sorted. When everything will be
@@ -208,119 +210,117 @@ int			ds(int *a_list, int *b_list)
 	 ** I'm sure it cant create confusion because med can only be positive
 	 */
 
-	while (med_table[1] != ENDED)
+	//	while (med_table[1] != ENDED)
+	//	{
+	/* Creating all the different med. And sorting our B_list into 'tas' */
+	//		while (b_list[0] >= 2)
+	//		{
+	//			++med_table[MED_NUM];
+	//			med_table[med_table[MED_NUM]] = (med_table[med_table[MED_NUM] - 1] / 2);
+	//			ft_printf("Med_table :");
+	//			ft_printint(med_table);
+	//			init = b_list[1];
+	//			while (b_list[b_list[0]] != init)
+	//			{
+	//				if (b_list[b_list[0]] > med_table[med_table[MED_NUM]] && ft_publish(PA) != S_ERR)
+	//					pa(a_list, b_list);
+	//				else if (ft_publish(RB) != S_ERR)
+	//					rb(b_list);
+	//			}
+	//		}
+	/* Why decrease med_table, because there is an empty case at the end.
+	*/
+	//		--med_table[MED_NUM];
+
+	/* We just clear the first stack in b */
+
+	//		if (b_list[0] != 1)
+	//		{
+	while (a_list[a_list[0]] < summ / 2)
 	{
-		/* Creating all the different med. And sorting our B_list into 'tas' */
-		while (b_list[0] >= 2)
-		{
-			++med_table[MED_NUM];
-			med_table[med_table[MED_NUM]] = (med_table[med_table[MED_NUM] - 1] / 2);
-			ft_printf("Med_table :");
-			ft_printint(med_table);
-			init = b_list[1];
-			while (b_list[b_list[0]] != init)
-			{
-				if (b_list[b_list[0]] > med_table[med_table[MED_NUM]] && ft_publish(PA) != S_ERR)
-					pa(a_list, b_list);
-				else if (ft_publish(RB) != S_ERR)
-					rb(b_list);
-			}
-		}
-		/* Why decrease med_table, because there is an empty case at the end.
-		*/
-		--med_table[MED_NUM];
-
-		/* We just clear the first stack in b */
-
-		if (b_list[0] != 1)
-		{
-			while (a_list[a_list[0]] < summ / 2)
-			{
-				while (a_list[a_list[0]] < (summ / 2) && ft_publish(PB) != S_ERR)
-					pb(a_list, b_list);
-				if (b_list[0] < LOW_SIZE_TO_SORT)
-					selective_sort(a_list, b_list);
-				else
-					parse_again_with_medians(a_list, b_list);
-			}
-			ft_printf("[END]\na_list :");
-			ft_printint(a_list);
-			ft_printf("b_list :");
-			ft_printint(b_list);
-			while(a_list[a_list[0]] > summ / 2)
-			{
-				while (a_list[a_list[0]] >= (summ / 2) && ft_publish(PB) != S_ERR)
-					pb(a_list, b_list);
-				if (b_list[0] < LOW_SIZE_TO_SORT)
-					selective_sort(a_list, b_list);
-				else
-					parse_again_with_medians(a_list, b_list);
-			}
-			ft_printint(a_list);
-			return (SORTED);
-		}
-		else if (ft_publish(PA) != S_ERR && ft_publish(RA) != S_ERR)
-		{
-			pa(a_list, b_list);
-			ra(a_list);
-			--med_table[MED_NUM];
-			med_table[med_table[MED_NUM] + 1] = 0;
-		}
-
-		/* Finally we are going to push what we got in A under our mediane */
-
-		ft_printf("Med Table :");
-		ft_printint(med_table);
-		ft_printf("Liste A :");
-		ft_printint(a_list);
-		ft_printf("Liste B :");
-		ft_printint(b_list);
-		while (med_table[MED_NUM] > 1)
-		{
-			while (a_list[a_list[0]] < med_table[med_table[MED_NUM]] && ft_publish(PB) != S_ERR)
-				pb(a_list, b_list);
-			if (b_list[0] < LOW_SIZE_TO_SORT)
-				selective_sort(a_list, b_list);
-			else
-			{
-				ft_printf("First launch of new feature\nb_list = ");
-				ft_printint(a_list);
-				parse_again_with_medians(a_list, b_list);
-			}
-			ft_printf("ITTERATIVE MED_TABLE =");
-			ft_printint(med_table);
-			if (b_list[0] != EMPTY)
-			{
-				ft_printf("[Etat avant crash]:\n\n");
-				ft_printf("Med_Table :");
-				ft_printint(med_table);
-				ft_printf("A_list :");
-				ft_printint(a_list);
-				ft_printf("B_list :");
-				ft_printint(b_list);
-				return (ft_putsterr(UNKNOWN_BUG));
-			}
-			else if (--med_table[MED_NUM])
-				med_table[med_table[MED_NUM] + 1] = 0;
-		}
-
-		/* WE MADE THE FIRST HALF MED TABLE CONTAINS ONLY 1 SIZE
-		 ** WHICH IS THE TOTAL SIZE
-		 */
-
-		/* For me in future, if you need to get the last one not sorted just go to 0 and
-		 ** take the next one zero, then you just have to push until 0 show up at the END
-		 ** of the list, you'll know that you pushed enough
-		 */
-
-
-		/* Where are we? We sorted the first list and pushed back our b_list into A.
-		 ** Now it's time to push what we had in b in A and put it at the beginning.
-		 */
+		while (a_list[a_list[0]] < (summ / 2) && ft_publish(PB) != S_ERR)
+			pb(a_list, b_list);
+		if (b_list[0] < LOW_SIZE_TO_SORT)
+			selective_sort(a_list, b_list);
+		else
+			parse_again_with_medians(a_list, b_list);
 	}
-	ft_printf("a_list :");
-	ft_printint(a_list);
-	ft_printf("b_list :");
-	ft_printint(b_list);
+	//			ft_printf("[END]\na_list :");
+	//			ft_printint(a_list);
+	//			ft_printf("b_list :");
+	//			ft_printint(b_list);
+	while(a_list[a_list[0]] > summ / 2)
+	{
+		while (a_list[a_list[0]] >= (summ / 2) && ft_publish(PB) != S_ERR)
+			pb(a_list, b_list);
+		if (b_list[0] < LOW_SIZE_TO_SORT)
+			selective_sort(a_list, b_list);
+		else
+			parse_again_with_medians(a_list, b_list);
+	}
 	return (SORTED);
+/*	else if (ft_publish(PA) != S_ERR && ft_publish(RA) != S_ERR)
+	{
+		pa(a_list, b_list);
+		ra(a_list);
+		--med_table[MED_NUM];
+		med_table[med_table[MED_NUM] + 1] = 0;
+	}*/
+
+	/* Finally we are going to push what we got in A under our mediane */
+
+	//		ft_printf("Med Table :");
+	//		ft_printint(med_table);
+	//		ft_printf("Liste A :");
+	//		ft_printint(a_list);
+	//		ft_printf("Liste B :");
+	//		ft_printint(b_list);
+/*	while (med_table[MED_NUM] > 1)
+	{
+		while (a_list[a_list[0]] < med_table[med_table[MED_NUM]] && ft_publish(PB) != S_ERR)
+			pb(a_list, b_list);
+		if (b_list[0] < LOW_SIZE_TO_SORT)
+			selective_sort(a_list, b_list);
+		else
+		{
+			//				ft_printf("First launch of new feature\nb_list = ");
+			//				ft_printint(a_list);
+			parse_again_with_medians(a_list, b_list);
+		}
+		//			ft_printf("ITTERATIVE MED_TABLE =");
+		//			ft_printint(med_table);
+		if (b_list[0] != EMPTY)
+		{
+			//				ft_printf("[Etat avant crash]:\n\n");
+			//				ft_printf("Med_Table :");
+			//				ft_printint(med_table);
+			//				ft_printf("A_list :");
+			//				ft_printint(a_list);
+			//				ft_printf("B_list :");
+			//				ft_printint(b_list);
+			return (ft_putsterr(UNKNOWN_BUG));
+		}
+		else if (--med_table[MED_NUM])
+			med_table[med_table[MED_NUM] + 1] = 0;
+	}*/
+
+	/* WE MADE THE FIRST HALF MED TABLE CONTAINS ONLY 1 SIZE
+	 ** WHICH IS THE TOTAL SIZE
+	 */
+
+	/* For me in future, if you need to get the last one not sorted just go to 0 and
+	 ** take the next one zero, then you just have to push until 0 show up at the END
+	 ** of the list, you'll know that you pushed enough
+	 */
+
+
+	/* Where are we? We sorted the first list and pushed back our b_list into A.
+	 ** Now it's time to push what we had in b in A and put it at the beginning.
+	 */
+//}
+//	ft_printf("a_list :");
+//	ft_printint(a_list);
+//	ft_printf("b_list :");
+//	ft_printint(b_list);
+return (SORTED);
 }
