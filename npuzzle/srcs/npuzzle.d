58f90a525f79d230bbbbf486d0ea5574a551cd33
Jules Spender <juspende@e1z2r12p3.42.us.org>
2019-03-29 03:57:41 -0700

    Adding size return to main

--- npuzzle/srcs/npuzzle.d
+++
@@ -1,4 +0,0 @@
-srcs/npuzzle.o: srcs/npuzzle.c srcs/../includes/npuzzle.h \
-  srcs/../includes/../libft/libft.h \
-  srcs/../includes/../free_all/include/ft_freeall.h \
-  srcs/../includes/../printf/include/ft_printf.h
--- npuzzle/srcs/npuzzle_parser.d
+++
@@ -1,4 +0,0 @@
-srcs/npuzzle_parser.o: srcs/npuzzle_parser.c srcs/../includes/npuzzle.h \
-  srcs/../includes/../libft/libft.h \
-  srcs/../includes/../free_all/include/ft_freeall.h \
-  srcs/../includes/../printf/include/ft_printf.h
